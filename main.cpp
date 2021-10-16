2d984b33b249a30241d22a97312a908e002ee0c6
belkiz-ozbek <belkizozbek9@gmail.com>
2021-10-16 22:29:06 +0300

    Create main.cpp
    Question1 added.

---
+++ main.cpp
@@ -0,0 +1,48 @@
+/*Purpose= to define six different variables and assign them some 
+values, and to do arithmetical operations*/
+#include <stdio.h>
+#define P 26.8934
+
+int main(void)
+
+{
+	
+	int x;
+	int y=32;
+	int z;
+	int summation;
+	
+	//Assignþdjgaerlgiþegölirhö
+	x=12;
+
+	//Get input variable
+	printf("Enter a number :");
+	scanf ("%d", &z );
+	
+	//Do addition 
+	summation =x+y+z;
+	
+	//Display result
+	printf("x+y+z is %d\n ", summation);
+	
+	// Çarpma
+	
+	double n;
+	double multiply;
+	
+	n=2.0; 
+	
+	//Do multiplication
+	multiply=P*n;
+	
+	
+	printf("P*n is %f\n", multiply);	
+	
+	return 5;
+}
+
+
+
+
+
+
